46cc974fa85ade22f38c37ce34d7fe1c8ce6b30a
mdadil_proven <madil@provensolution.com>
2021-02-04 10:34:41 +0530

    initial commit

--- DataStructures/src/Node.kt
+++
@@ -1,8 +0,0 @@
-data class Node<T>(var info : T,
-                   var prev : Node<T>? = null,
-                   var next : Node<T>? = null){
-
-    override fun toString():String{
-        return "$info -> $next"
-    }
-}
